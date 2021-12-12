0065c96d2e03f01a9af4b19a51e59751debbde3c
Sergei Komissarikhin <Komissarihin.s@yandex.ru>
2021-12-12 22:59:22 +0300

    Begin connect JS from home to work

--- src/main/resources/static/js/MyScript.js
+++ src/main/resources/static/js/MyScript.js
@@ -1,22 +1,23 @@
-const url ='http://localhost:8080/api/users'
+const url = 'http://localhost:8080/api/users'
 console.log(url)
 const allUsers = document.getElementById('allUsers')
 let output = ''
 fetch(url)
     .then(res => res.json())
-    .then((data) =>{
-    data.forEach(post => {
-output += `
-<tr>
+    .then((data) => {
+        data.forEach(post => {
+            console.log(post.roles);
+            output += `
+            <tr>
                 <td id="id${post.id}">${post.id}</td>
                 <td id="firstName{post.id}">${post.firstName}</td>
                 <td id="lastName{post.id}">${post.lastName}</td>
                 <td id="age{post.id}">${post.age}</td>
                 <td id="email{post.id}">${post.email}</td>
-                <td id="roles${post.id}">${post.role}</td>
-                </tr>             
+                <td id="roles${post.id}">${post.roles.map(r => r.role).join(', ')}</td>
+            </tr>             
 `
-    })
+        })
         allUsers.innerHTML = output
 
-})
+    }).then(data => console.log(data))
