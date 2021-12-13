4eaf791724caccbace892e54cb6a5deaa6c6c3e2
Sergei Komissarikhin <Komissarihin.s@yandex.ru>
2021-12-13 22:39:32 +0300

    Begin connect JS from home to work2

--- src/main/resources/static/js/MyScript.js
+++ src/main/resources/static/js/MyScript.js
@@ -15,9 +15,42 @@ fetch(url)
                 <td id="age{post.id}">${post.age}</td>
                 <td id="email{post.id}">${post.email}</td>
                 <td id="roles${post.id}">${post.roles.map(r => r.role).join(', ')}</td>
+                <td>
+                                                <!-- Button edit modal -->
+                    <button type="button" class="btn btn-info" data-toggle="modal"
+                            data-target="#edit" onclick="edit(${post.id})"
+                            th:attrappend="data-target=${post.id}">
+                        Edit
+                    </button>
+                 </td>
+                 <td>
+                                                <!-- Button delete modal -->
+                    <button type="button" class="btn btn-danger" data-toggle="modal"
+                            data-target="#delete" onclick="del(${post.id})"
+                            th:attrappend="data-target=${post.id}">
+                        Delete
+                    </button>
+                 </td>
             </tr>             
 `
         })
         allUsers.innerHTML = output
 
     }).then(data => console.log(data))
+
+function edit(id){
+    fetch(`${url}/${id}`)
+        .then(res => res.json())
+        .then(user => {
+            console.log(user)
+            }
+        )
+}
+function del(id){
+    fetch(`${url}/${id}`)
+        .then(res => res.json())
+        .then(user => {
+                console.log(user)
+            }
+        )
+}
